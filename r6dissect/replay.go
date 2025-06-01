672aa0abb39d7c7fbc29fd27f09202827be4d800
pmezzich <pmezzich@gmail.com>
2025-06-01 13:34:50 -0400

    Add files via upload

---
+++ r6dissect/replay.go
@@ -0,0 +1,27 @@
+package replay
+
+import (
+    "os"
+    "r6dissect/reader"
+    "r6dissect/match"
+)
+
+func ParseMatchReplay(filePath string) (*match.Match, error) {
+    f, err := os.Open(filePath)
+    if err != nil {
+        return nil, err
+    }
+    defer f.Close()
+
+    parsedReader, err := reader.ReadReplay(f)
+    if err != nil {
+        return nil, err
+    }
+
+    parsedMatch, err := match.Parse(parsedReader)
+    if err != nil {
+        return nil, err
+    }
+
+    return parsedMatch, nil
+}
