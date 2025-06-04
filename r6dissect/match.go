25d3426efb69e377bfd7de31dd618b02e3fd4441
pmezzich <pmezzich@gmail.com>
2025-06-04 01:05:01 -0400

    Update match.go

--- r6dissect/match.go
+++ r6dissect/match.go
@@ -1,14 +1,18 @@
 package r6dissect
 
 type PlayerRoundStats struct {
-    Name     string `json:"name"`
-    Kills    int    `json:"kills"`
-    Deaths   int    `json:"deaths"`
-    Assists  int    `json:"assists"`
-    Headshot int    `json:"headshot"`
-    Plants   int    `json:"plants"`
-    Trades   int    `json:"trades"`
+	Name     string `json:"name"`      // Player's name
+	Kills    int    `json:"kills"`     // Number of kills
+	Deaths   int    `json:"deaths"`    // Number of deaths
+	Assists  int    `json:"assists"`   // Number of assists
+	Headshot int    `json:"headshot"`  // Number of headshots
+	Plants   int    `json:"plants"`    // Number of plants
+	Trades   int    `json:"trades"`    // Number of trades
+	Score    int    `json:"score"`     // Optional: Total round score
+	Team     int    `json:"team"`      // Optional: Team index
+	Operator string `json:"operator"`  // Optional: Operator used
 }
+
 
 type Round struct {
     Map         string             `json:"map"`
